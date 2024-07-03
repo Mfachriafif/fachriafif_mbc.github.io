308ab4a0163e111ade30f9027b456c0e77d8454b
Mfachriafif <fachryafif86@gmail.com>
2024-07-03 17:07:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Slide
+var slideIndex = 0;
+showSlides();
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName('mySlides');
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex].style.display = 'block';
+  setTimeout(showSlides, 2000);
+}
